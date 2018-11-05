07a2429007858dade19f3be71507e633fc32c854
Daniel Wittenberg <dan.wittenberg@thinkahead.com>
2018-11-05 11:17:29 -0600

    initial import

---
+++ install-myvim.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -m 755 -p $HOME/.vim/bundle
+cd $HOME/.vim/bundle
+
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/godlygeek/tabular.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/tpope/vim-git.git
+git clone https://github.com/rodjek/vim-puppet.git
+git clone https://github.com/vim-ruby/vim-ruby.git
+git clone https://github.com/astashov/vim-ruby-debugger.git
+git clone https://github.com/garbas/vim-snipmate.git
+git clone https://github.com/honza/vim-snippets.git
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/jnurmine/Zenburn.git
